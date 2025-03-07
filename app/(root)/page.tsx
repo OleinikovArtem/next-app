79525efa4c91ba3c317233d8b79d33702790f686
ArtOleinikov <aoleinikov@wiley.com>
2025-03-07 11:18:10 +0200

    add Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
-  console.log('log')
-
   return (
     <>
-      <h1 className="text-2xl">Welcome Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
     </>
   );
 }
