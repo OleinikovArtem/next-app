7453491704dd6037f28b478184d26c6d717e0de3
ArtOleinikov <artem.oleinikov.web@gmail.com>
2025-03-11 13:29:55 +0200

    remove Dashboard

--- app/(dashboard)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-  return (
-    <>
-      <h1 className="text-3xl">Dashboard navbar</h1>
-      {children}
-    </>
-  );
-}
